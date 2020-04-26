671b7c6649ef14a9c25b6adab63368b916d10f6f
weiyafei <2553241022@qq.com>
2020-04-26 15:28:33 +0800

    测试HEAD

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -11,7 +11,7 @@ axios.defaults.headers.post["Content-Type"] =
 
 let config = {
   baseURL: process.env.baseURL || process.env.apiUrl || "",
-  // timeout: 60 * 1000, // Timeout
+  timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
 
