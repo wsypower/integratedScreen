c54cb0bd7ed037f66a1027a942812982ec0e0613
weiyafei <2553241022@qq.com>
2020-04-30 16:29:03 +0800

    测试merge

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
-import './plugins/axios'
+import "./plugins/axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import './plugins/element.js'
-
+import "./plugins/element.js";
+//1
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
